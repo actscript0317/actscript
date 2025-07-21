a85fbeb5a36be45e3980dc4f89e266c0e73ab478
Jun Jongchun <actscript0317@gmail.com>
2025-07-21 18:19:55 +0900

    ✨ 회원가입 기능 수정: user 저장 문제 해결

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config/env');
+const cors = require('cors');
 
 const userSchema = new mongoose.Schema({
   username: {
